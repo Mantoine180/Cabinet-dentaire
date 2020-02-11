1d41491b7891335c2f9e7b29f8f28a1e51c3e2c3
Mantoine180 <claverie.tonio@gmail.com>
2020-02-11 20:26:09 +0100

    javascript

---
+++ app.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper ('.swiper-container', {
+      loop:true,
+      
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      autoplay: {
+          delay:5000
+      }
+})
